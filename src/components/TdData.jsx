1c5dfaa98fb4010dcba54c34fc45b392b613eaa7
BranGitfox <vixfgit@gmail.com>
2024-11-21 16:43:44 +0300

    feat: wrapped td in a component

---
+++ src/components/TdData.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { FaCheckCircle, FaInfoCircle, FaSearch, } from "react-icons/fa";
+
+function TdData({data}) {
+  return (
+    <>
+        <tr  class="text-gray-700">
+                    <td class="px-4 py-3">
+                      
+                      <div class="flex items-center text-sm">
+                        <div>
+                          <p class="font-semibold">{data.ref}</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-4 py-3 text-sm">{data.mat}</td>
+                    <td class="px-4 py-3 text-sm">{data.dir}</td>
+                    <td class="px-4 py-3 text-sm">{data.prop}</td>
+                    <td class="px-4 py-3 text-sm">{data.objet}</td>
+                    <td class="px-4 py-3 text-xs">
+                      <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full ">
+                        Decharge
+                      </span>
+                    </td>
+                    <td class="px-4 py-3 text-xs">
+                      <FaCheckCircle className="text-green-600 text-xl" />
+                    </td>
+                    <td class="px-4 py-3 text-xs">
+                      <FaInfoCircle className="text-blue-00 text-xl" />
+                    </td>
+                  </tr>
+                  <tr>
+                      <td colSpan={8} className='text-center text-gray-800'>
+                        <h1>Hello world</h1>
+                      </td>
+                  </tr>
+    </>
+  )
+}
+
+export default TdData
--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
-import { FaCheckCircle, FaInfoCircle, FaSearch, } from "react-icons/fa";
+import { FaSearch, } from "react-icons/fa";
 import data from "../data";
+import TdData from '../components/TdData';
+
 
 
 function Tracker() {
@@ -41,43 +43,22 @@ function Tracker() {
             <table class="w-full whitespace-no-wrap">
               <thead>
                 <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b0 bg-gray-50 ">
-                  <th className="px-4 py-3">Reference</th>
-                  <th className="px-4 py-3">Matricules</th>
-                  <th className="px-4 py-3">Direction</th>
-                  <th className="px-4 py-3">Proprietaire</th>
-                  <th className="px-4 py-3">Objet</th>
-                  <th className="px-4 py-3">Status</th>
-                  <th className="px-4 py-3">Livre</th>
-                  <th className="px-4 py-3">Infos</th>
+                  <th className="px-4 py-3 text-gray-800">Reference</th>
+                  <th className="px-4 py-3 text-gray-800" >Matricules</th>
+                  <th className="px-4 py-3 text-gray-800">Direction</th>
+                  <th className="px-4 py-3 text-gray-800">Proprietaire</th>
+                  <th className="px-4 py-3 text-gray-800">Objet</th>
+                  <th className="px-4 py-3 text-gray-800">Status</th>
+                  <th className="px-4 py-3 text-gray-800">Livre</th>
+                  <th className="px-4 py-3 text-gray-800">Infos</th>
                 </tr>
               </thead>
               <tbody class="bg-white divide-y ">
                 {filtered.map((data, index) => (
-                  <tr key={index} class="text-gray-700">
-                    <td class="px-4 py-3">
-                      
-                      <div class="flex items-center text-sm">
-                        <div>
-                          <p class="font-semibold">{data.ref}</p>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-4 py-3 text-sm">{data.mat}</td>
-                    <td class="px-4 py-3 text-sm">{data.dir}</td>
-                    <td class="px-4 py-3 text-sm">{data.prop}</td>
-                    <td class="px-4 py-3 text-sm">{data.objet}</td>
-                    <td class="px-4 py-3 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full ">
-                        Decharge
-                      </span>
-                    </td>
-                    <td class="px-4 py-3 text-xs">
-                      <FaCheckCircle className="text-green-600 text-xl" />
-                    </td>
-                    <td class="px-4 py-3 text-xs">
-                      <FaInfoCircle className="text-blue-00 text-xl" />
-                    </td>
-                  </tr>
+                  <>
+                    <TdData data={data} key={index}/>
+                  </>
+                  
                 ))}
               </tbody>
             </table>
