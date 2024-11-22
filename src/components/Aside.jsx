139ed7f3fc525311d7a635a07e4a773995b982c1
BranGitfox <vixfgit@gmail.com>
2024-11-22 08:28:52 +0300

    refactor: changing link path

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -21,7 +21,7 @@ function Aside() {
           <ul>
             <li class="relative px-6 py-3 ">
                 {
-                    location.pathname ==='/scc/'?(   <span
+                    location.pathname ==='/scc'?(   <span
                 class="absolute inset-y-0 left-0 w-1 bg-[#C1AB48] rounded-tr-lg rounded-br-lg"
                 aria-hidden="true"
               ></span>):''
@@ -29,7 +29,7 @@ function Aside() {
          
               <Link
                 class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-300 "
-                to='/scc/'              >
+                to='/scc'              >
                 <svg
                   class="w-5 h-5"
                   aria-hidden="true"
