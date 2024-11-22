52969846475894bb5c9546c63b5461ba3e816db3
BranGitfox <vixfgit@gmail.com>
2024-11-22 08:58:52 +0300

    feat: adding burger menu

--- src/pages/Scc.jsx
+++ src/pages/Scc.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import {  FaUser } from "react-icons/fa";
+import {  FaHamburger, FaUser } from "react-icons/fa";
 import Aside from "../components/Aside";
 import { Outlet } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
 function Scc() {
  
 
@@ -11,7 +12,11 @@ function Scc() {
         <Aside />
         <div class="w-full md:overflow-hidden shadow-xs">
           <div className=" w-[100%] justify-between flex p-3 bg-gray-50 mb-5">
-            <h2 className="font-semibold text-gray-700">
+            <h2 className="font-semibold hidden md:block text-gray-700">
+              SCC
+            </h2>
+            <BiMenu className="text-gray-900 md:hidden" size={20}/>
+            <h2 className="font-semibold ml-16 md:hidden text-gray-700">
               SCC
             </h2>
             <h3 className="font-semibold text-gray-700">
