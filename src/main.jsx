a9e7879c0f8ed588bbd91e3729f90d9a19895ae0
BranGitfox <vixfgit@gmail.com>
2024-11-24 22:58:08 +0300

    refactor: adding index to route

--- src/main.jsx
+++ src/main.jsx
@@ -54,7 +54,5 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>
-      <App/>
-    <RouterProvider/>
   </StrictMode>,
 )
