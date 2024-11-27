d1de32a27431e71b2b054799945bdbe4b60099c9
MiotyRan <miotisoaranarimanana@gmail.com>
2024-11-27 09:20:07 +0300

    refactor: boutton déconnecter

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -117,7 +117,7 @@ function Aside({toggleMenu,menu, logout, loading}) {
             onClick={logout}
               class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-[#A10304] border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
             >
-             {loading?(<BeatLoader size={20} color='yellow'/>):'Se Deconnecter'}
+             {loading?(<BeatLoader size={20} color='yellow'/>):'Se Déconnecter'}
               <FaArrowRight/>
             </button>
           </div>
