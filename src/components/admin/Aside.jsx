effc6b6ce07b66333d25bfe8c87243ad9a1b95e7
BranGitfox <vixfgit@gmail.com>
2024-11-27 11:41:32 +0300

    feat: adding another link to admin page

--- src/components/admin/Aside.jsx
+++ src/components/admin/Aside.jsx
@@ -1,26 +1,33 @@
 
-import { FaDashcube, FaUserPlus } from 'react-icons/fa'
-import {FaUnlockKeyhole } from 'react-icons/fa6'
+import { FaChartLine, FaFlag, FaMailBulk, FaUserPlus } from 'react-icons/fa'
+import {FaHouseLaptop, FaUnlockKeyhole } from 'react-icons/fa6'
+import { Link } from 'react-router-dom'
 function Aside() {
   return (
    <>
-    <aside className="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
-        <div className="p-6">
-            <a href="index.html" className="text-white text-3xl font-semibold uppercase hover:text-gray-300"><FaUnlockKeyhole className='inline mr-3'/>Admin</a>
+    <aside className="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl bg-blue-900">
+        <div className="p-6 ">
+            <Link href="/admin" className="text-white text-3xl font-semibold uppercase hover:text-gray-300"><FaUnlockKeyhole className='inline mr-3 mb-2'/>Admin</Link>
         </div>
         <nav className="text-white text-base font-semibold pt-3">
-            <a href="index.html" className="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
-                <FaDashcube size={20} className='mr-3'/>
-                Dashboard
-            </a>
-            <a href="blank.html" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-               <FaUserPlus size={20} className='mr-3'/> Utilisateur
-            </a>
+            <Link href="index.html" className="flex items-center hover:bg-blue-800  text-white py-4 pl-6 nav-item">
+                <FaChartLine size={20} className='mr-3'/>
+                Tableau de Bord
+            </Link>
+            <Link href="blank.html" className="flex items-center text-white hover:bg-blue-800 py-4 pl-6 nav-item">
+               <FaUserPlus size={20} className='mr-3'/>Ajout Utilisateur
+            </Link>
+            <Link href="blank.html" className="flex items-center text-white hover:bg-blue-800 py-4 pl-6 nav-item">
+               <FaHouseLaptop size={20} className='mr-3'/>Ajout Dir/Serv
+            </Link>
+            <Link href="blank.html" className="flex items-center text-white hover:bg-blue-800 py-4 pl-6 nav-item">
+               <FaMailBulk size={20} className='mr-3'/>Visiter Service-CC
+            </Link>
         </nav>
-        <a href="#" className="absolute w-full upgrade-btn bottom-0 active-nav-link text-white flex items-center justify-center py-4">
-            <i className="fas fa-arrow-circle-up mr-3"></i>
+        <h2 href="#" className="absolute w-full font-bold    bottom-0  text-white flex items-center justify-center py-4">
+            <FaFlag className='mr-3'/>
             E-parapheur
-        </a>
+        </h2>
     </aside>
    </>
   )
