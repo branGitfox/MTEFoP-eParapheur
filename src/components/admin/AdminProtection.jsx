8468e2fada74cddce33d325df68e3a88203e1a3d
BranGitfox <vixfgit@gmail.com>
2024-11-27 11:43:13 +0300

    feat: adding admin secure component

---
+++ src/components/admin/AdminProtection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+/**
+ *Protection de la page admin
+ *
+ * @return {*} 
+ */
+function AdminProtection() {
+  return (
+    <div>AdminProtection</div>
+  )
+}
+
+export default AdminProtection
