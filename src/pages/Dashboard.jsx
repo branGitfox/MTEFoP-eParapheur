e924f96b6588d0a12fcd2bfb35b5180eed49e7d5
BranGitfox <vixfgit@gmail.com>
2024-11-29 22:28:59 +0300

    feat: design dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -47,6 +47,45 @@ function Dashboard() {
                     </div>
                 </div>
             </div>
+            <div class="w-[300px]">
+                <div class="border-2 border-gray-400 border-dashed hover:border-transparent hover:bg-white hover:shadow-xl rounded p-6 m-2 md:mx-10 md:my-6">
+                    <div class="flex flex-col items-center">
+                        <div class="flex-shrink pr-4">
+                            <div class="rounded-full p-3 bg-gray-300"><FaWallet className='text-amber-500'/></div>
+                        </div>
+                        <div class="flex-1">
+                            <h3 class="font-bold text-3xl text-gray-900">3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                            <h5 class="font-bold text-gray-500">Total Document</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-[300px]">
+                <div class="border-2 border-gray-400 border-dashed hover:border-transparent hover:bg-white hover:shadow-xl rounded p-6 m-2 md:mx-10 md:my-6">
+                    <div class="flex flex-col items-center">
+                        <div class="flex-shrink pr-4">
+                            <div class="rounded-full p-3 bg-gray-300"><FaWallet className='text-amber-500'/></div>
+                        </div>
+                        <div class="flex-1">
+                            <h3 class="font-bold text-3xl text-gray-900">3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                            <h5 class="font-bold text-gray-500">Total Document</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-[300px]">
+                <div class="border-2 border-gray-400 border-dashed hover:border-transparent hover:bg-white hover:shadow-xl rounded p-6 m-2 md:mx-10 md:my-6">
+                    <div class="flex flex-col items-center">
+                        <div class="flex-shrink pr-4">
+                            <div class="rounded-full p-3 bg-gray-300"><FaWallet className='text-amber-500'/></div>
+                        </div>
+                        <div class="flex-1">
+                            <h3 class="font-bold text-3xl text-gray-900">3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                            <h5 class="font-bold text-gray-500">Total Document</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
            
         </div>
         <hr className='text-gray-900'/>
