95a4500c1829b697f7e878530644b943954fd8fc
BranGitfox <vixfgit@gmail.com>
2024-11-30 23:22:04 +0300

    refactor: changing class to classname

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,25 +5,25 @@ function Register() {
     <>
         <h3 className='text-gray-900 text-2xl ml-2.5 font-semibold '>Enregistrement d'un Dossier</h3>
 <form className='w-[100%]  m-auto  p-10 bg-white rounded-md shadow mt-5 md:mt-10' >
-    <div class="grid gap-6 mb-1 md:grid-cols-2">
+    <div className="grid gap-6 mb-1 md:grid-cols-2">
         <div>
-            <label for="ref" class="block mb-2 text-sm font-medium text-gray-900 ">Reference</label>
-            <input type="text" id="ref" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="reference du dossier" required />
+            <label for="ref" className="block mb-2 text-sm font-medium text-gray-900 ">Reference</label>
+            <input type="text" id="ref" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="reference du dossier" required />
         </div>
         <div>
-            <label for="chrono" class="block mb-2 text-sm font-medium text-gray-900 ">Chrono</label>
-            <input type="text" id="Chrono" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="chrono initial" required />
+            <label for="chrono" className="block mb-2 text-sm font-medium text-gray-900 ">Chrono</label>
+            <input type="text" id="Chrono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="chrono initial" required />
         </div>
         <div>
-            <label for="prop" class="block mb-2 text-sm font-medium text-gray-900 ">Proprietaire</label>
-            <input type="text" id="prop" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="proprietaire du dossier" required />
+            <label for="prop" className="block mb-2 text-sm font-medium text-gray-900 ">Proprietaire</label>
+            <input type="text" id="prop" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="proprietaire du dossier" required />
         </div>  
         <div>
-            <label for="objet" class="block mb-2 text-sm font-medium text-gray-900 ">Objet</label>
-            <input type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   " placeholder="objet du depot"  required />
+            <label for="objet" className="block mb-2 text-sm font-medium text-gray-900 ">Objet</label>
+            <input type="text" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   " placeholder="objet du depot"  required />
         </div>
         <div>
-            <label for="website" class="block mb-2 text-sm font-medium text-gray-900 ">Direction</label>
+            <label for="website" className="block mb-2 text-sm font-medium text-gray-900 ">Direction</label>
             <select className='text-gray-900 w-full p-2 rounded' name="" id="">
                 <option value="">DRFP</option>
                 <option value="">DSI</option>
@@ -32,14 +32,14 @@ function Register() {
             </select>
         </div>
         <div>
-            <label for="date" class="block mb-2 text-sm font-medium text-gray-900 ">Date D'ajout</label>
+            <label for="date" className="block mb-2 text-sm font-medium text-gray-900 ">Date D'ajout</label>
                 <input type="date" name="" id="date" className='text-black w-full border p-1 rounded' />
             </div>
     </div>
     
     
 
-    <button type="submit" class="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-10">Enregistrer</button>
+    <button type="submit" className="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-10">Enregistrer</button>
 </form>
 
     </>
