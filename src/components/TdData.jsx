c8f7fe892d1be8bc3b3b2bd9a6af3d28a3bfd9d5
BranGitfox <vixfgit@gmail.com>
2024-11-30 23:24:28 +0300

    refactor: changing class to classname

--- src/components/TdData.jsx
+++ src/components/TdData.jsx
@@ -10,28 +10,28 @@ function TdData({data}) {
     }
   return (
     <>
-        <tr  class="text-gray-700">
-                    <td class="px-4 py-3">
+        <tr  className="text-gray-700">
+                    <td className="px-4 py-3">
                       
-                      <div class="flex items-center text-sm">
+                      <div className="flex items-center text-sm">
                         <div>
-                          <p class="font-semibold">{data.ref}</p>
+                          <p className="font-semibold">{data.ref}</p>
                         </div>
                       </div>
                     </td>
-                    <td class="px-4 py-3 text-sm">{data.mat}</td>
-                    <td class="px-4 py-3 text-sm">{data.dir}</td>
-                    <td class="px-4 py-3 text-sm">{data.prop}</td>
-                    <td class="px-4 py-3 text-sm">{data.objet}</td>
-                    <td class="px-4 py-3 text-xs">
-                      <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full ">
+                    <td className="px-4 py-3 text-sm">{data.mat}</td>
+                    <td className="px-4 py-3 text-sm">{data.dir}</td>
+                    <td className="px-4 py-3 text-sm">{data.prop}</td>
+                    <td className="px-4 py-3 text-sm">{data.objet}</td>
+                    <td className="px-4 py-3 text-xs">
+                      <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full ">
                         Decharge
                       </span>
                     </td>
-                    <td class="px-4 py-3 text-xs">
+                    <td className="px-4 py-3 text-xs">
                       <FaCheckCircle className="text-green-600 text-xl" />
                     </td>
-                    <td class="px-4 py-3 text-xs">
+                    <td className="px-4 py-3 text-xs">
                         {showInfo === true? <FaArrowUp onClick={toggleShow} className="text-gray-400 text-xl" />: <BiDotsHorizontal onClick={toggleShow} className="text-gray-400 text-xl" />}
                      
                     </td>
