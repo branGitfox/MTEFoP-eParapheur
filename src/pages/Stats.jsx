50f952762a85c9edf5b599d66927ce529a9d3ff8
BranGitfox <vixfgit@gmail.com>
2024-11-30 23:22:39 +0300

    refactor: changing class to classname

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -17,47 +17,47 @@ function Stats() {
                 </select>
         </div>
    
-        <div class="flex flex-wrap">
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+        <div className="flex flex-wrap">
+                <div className="w-full md:w-1/2 xl:w-1/3 p-3">
                    
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-blue-800"><SiPaperswithcode/></div>
+                    <div className="bg-white border rounded shadow p-2">
+                        <div className="flex flex-row items-center">
+                            <div className="flex-shrink pr-4">
+                                <div className="rounded p-3 bg-blue-800"><SiPaperswithcode/></div>
                             </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">Total Dossiers</h5>
-                                <h3 class="font-bold text-3xl text-gray-900">3249 <span class="text-blue-500"><i class="fas fa-caret-up"></i></span></h3>
-                            </div>
-                        </div>
-                    </div>
-                   
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                   
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-orange-600"><SiPinboard/></div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">Total En Attente</h5>
-                                <h3 class="font-bold text-3xl text-gray-900">3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                            <div className="flex-1 text-right md:text-center">
+                                <h5 className="font-bold uppercase text-gray-500">Total Dossiers</h5>
+                                <h3 className="font-bold text-3xl text-gray-900">3249 <span className="text-blue-500"><i className="fas fa-caret-up"></i></span></h3>
                             </div>
                         </div>
                     </div>
                    
                 </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+                <div className="w-full md:w-1/2 xl:w-1/3 p-3">
                    
-                    <div class="bg-white border rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-yellow-600"><FaThumbsUp/></div>
+                    <div className="bg-white border rounded shadow p-2">
+                        <div className="flex flex-row items-center">
+                            <div className="flex-shrink pr-4">
+                                <div className="rounded p-3 bg-orange-600"><SiPinboard/></div>
                             </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <h5 class="font-bold uppercase text-gray-500">Total Livre</h5>
-                                <h3 class="font-bold text-3xl text-gray-900">3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                            <div className="flex-1 text-right md:text-center">
+                                <h5 className="font-bold uppercase text-gray-500">Total En Attente</h5>
+                                <h3 className="font-bold text-3xl text-gray-900">3249 <span className="text-green-500"><i className="fas fa-caret-up"></i></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+                <div className="w-full md:w-1/2 xl:w-1/3 p-3">
+                   
+                    <div className="bg-white border rounded shadow p-2">
+                        <div className="flex flex-row items-center">
+                            <div className="flex-shrink pr-4">
+                                <div className="rounded p-3 bg-yellow-600"><FaThumbsUp/></div>
+                            </div>
+                            <div className="flex-1 text-right md:text-center">
+                                <h5 className="font-bold uppercase text-gray-500">Total Livre</h5>
+                                <h3 className="font-bold text-3xl text-gray-900">3249 <span className="text-green-500"><i className="fas fa-caret-up"></i></span></h3>
                             </div>
                         </div>
                     </div>
