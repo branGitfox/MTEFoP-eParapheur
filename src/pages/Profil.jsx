4bd06311d4817b83493fc1896373419bda4bea2d
BranGitfox <vixfgit@gmail.com>
2024-12-01 20:08:59 +0300

    feat: adding user email

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function Profil() {
   return (
     <>
-      <div className="-w-full p-5 flex justify-center">
+      <div className="-w-full p-5 flex justify-center flex-col items-center">
         <div className="mb-5 w-[250px] h-[250px] rounded-full border-2 border-blue-900"></div>
+        <h2 className="text-gray-900 font-semibold">Vixfgit@gmail.com </h2>
       </div>
     </>
   );
