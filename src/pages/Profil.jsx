42f6540530c072e85228b64c04958f8024b69d2c
BranGitfox <vixfgit@gmail.com>
2024-12-01 20:12:51 +0300

    feat: adding hr devided

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -6,6 +6,8 @@ function Profil() {
       <div className="-w-full p-5 flex justify-center flex-col items-center">
         <div className="mb-5 w-[250px] h-[250px] rounded-full border-2 border-blue-900"></div>
         <h2 className="text-gray-900 font-semibold">Vixfgit@gmail.com </h2>
+        <hr  className="mt-5 mb-5 text-gray-900 w-full"/>
+        <h3 className="">Informations</h3>
       </div>
     </>
   );
