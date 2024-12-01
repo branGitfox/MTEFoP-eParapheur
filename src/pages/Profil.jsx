29e0e3d429adf6bcfc4af61e866f09763e59506f
BranGitfox <vixfgit@gmail.com>
2024-12-01 20:05:21 +0300

    feat: adding progil picture container

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Profil() {
   return (
-    <div>Profil</div>
-  )
+    <>
+      <div className="-w-full p-5 flex justify-center">
+        <div className="mb-5 w-[250px] h-[250px] rounded-full border-2 border-blue-900"></div>
+      </div>
+    </>
+  );
 }
 
-export default Profil
+export default Profil;
