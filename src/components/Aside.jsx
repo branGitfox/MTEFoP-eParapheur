f13aaef8fede45c6c230ea21b10cac6e8cb94451
BranGitfox <vixfgit@gmail.com>
2024-12-01 19:49:35 +0300

    refactor: deleting non used comment

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -110,12 +110,7 @@ function Aside({toggleMenu,menu, logout, loading, user}) {
               </Link>
             </li>
             {user.role=='admin'? (            <li className="relative px-6 py-3">
-            {/* {
-                    location.pathname ==='/scc/stats'?(   <span
-                className="absolute inset-y-0 left-0 w-1 bg-[#C1AB48] rounded-tr-lg rounded-br-lg"
-                aria-hidden="true"
-              ></span>):''
-                } */}
+              
               <Link
                 className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-300 "
                 to='/admin'
