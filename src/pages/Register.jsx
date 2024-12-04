99c26cdfa82b93e9885578faeb31ba25ee605cb7
BranGitfox <vixfgit@gmail.com>
2024-12-04 10:53:09 +0300

    feat(register): adding BeatLoader to save btn

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -70,7 +70,7 @@ function Register() {
                 <option value="">DMI</option>
             </select>
         </div>
-    <button type="submit" className="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-10">Enregistrer</button>
+    <button type="submit" className="text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-10"> {isLoading ? <BeatLoader color="yellow" /> : "Enregistrer"}</button>
 </form>
     </>
   )
