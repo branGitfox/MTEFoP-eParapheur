eda6e14307c6aa68f3574a81b1d1659c1ff1de48
BranGitfox <vixfgit@gmail.com>
2024-12-04 20:42:19 +0300

    refactor: changing the blue palette color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ function Nav() {
     <>
     
 
-    <nav id="header" className="fixed w-full z-50 top-0 text-white bg-blue-900">
+    <nav id="header" className="fixed w-full z-50 top-0 text-white bg-[#191970]">
     <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2 -z-50">
       <div className="pl-4 flex items-center">
         <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
