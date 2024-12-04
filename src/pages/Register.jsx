1c8a085377198d388197abbff3042bbb12f175a9
BranGitfox <vixfgit@gmail.com>
2024-12-04 09:56:06 +0300

    refactor: changing and adding fields to register doc

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,26 +5,36 @@ function Register() {
     <>
         <h3 className='text-gray-900 text-2xl ml-2.5 font-semibold '>Enregistrement d'un Dossier</h3>
 <form className='w-[100%]  m-auto  p-10 bg-white rounded-md shadow mt-5 md:mt-10' >
-    {/* <div className="grid gap-6 mb-1 md:grid-cols-2"> */}
         <div className='mb-5'>
-            <label htmlFor="ref" className="block mb-2 text-sm font-medium text-gray-900 ">Reference</label>
-            <input type="text" id="ref" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="reference du dossier" required />
+            <label htmlFor="ref" className="block mb-2 text-sm font-medium text-gray-900 ">Chrono</label>
+            <input type="text" id="ref" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="reference du dossier" name='chrono'/>
         </div>
         <div className='mb-5'>
-            <label htmlFor="chrono" className="block mb-2 text-sm font-medium text-gray-900 ">Chrono</label>
-            <input type="text" id="Chrono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="chrono initial" required />
+            <label htmlFor="chrono" className="block mb-2 text-sm font-medium text-gray-900 ">Reference</label>
+            <input type="text" id="Chrono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="chrono initial" name='ref'/>
+        </div>
+        <div className='mb-5'>
+            <label htmlFor="chrono" className="block mb-2 text-sm font-medium text-gray-900 ">Provenance</label>
+            <input type="text" id="Chrono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="Provenance" name='Provenance'/>
         </div>
         <div className='mb-5'>
             <label htmlFor="prop" className="block mb-2 text-sm font-medium text-gray-900 ">Proprietaire</label>
-            <input type="text" id="prop" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="proprietaire du dossier" required />
+            <input type="text" id="prop" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="proprietaire du dossier" name='propr'/>
         </div>  
         <div className='mb-5'>
-            <label htmlFor="objet" className="block mb-2 text-sm font-medium text-gray-900 ">Objet</label>
-            <input type="text" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   " placeholder="objet du depot"  required />
+            <label htmlFor="objet" className="block mb-2 text-sm font-medium text-gray-900 ">Motif</label>
+            <input type="text" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   " placeholder="objet du depot"  name='motif' />
         </div >
         <div className='mb-5'>
+            <label htmlFor="website" className="block mb-2 text-sm font-medium text-gray-900 ">Caracteristique</label>
+            <select className='text-gray-900 w-full p-2 rounded' name="caracteristique" id="">
+                <option value="">- Selectionner ici -</option>
+                <option value="plis ferme">Plis Ferme</option>
+            </select>
+        </div>
+        <div className='mb-5'>
             <label htmlFor="website" className="block mb-2 text-sm font-medium text-gray-900 ">Direction</label>
-            <select className='text-gray-900 w-full p-2 rounded' name="" id="">
+            <select className='text-gray-900 w-full p-2 rounded' name="dir" id="">
                 <option value="">DRFP</option>
                 <option value="">DSI</option>
                 <option value="">DRHE</option>
