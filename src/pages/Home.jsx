e34909cb1f6739d10815e7d398455bfbb53c861f
BranGitfox <vixfgit@gmail.com>
2024-12-05 09:41:32 +0300

    style: adding fadeInRight to Hero img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
           </Link>
         </div>
         <div className="w-full md:w-3/5 py-6 text-right relative ">
-          <img className="w-full md:w-4/5 z-50 relative lg:left-[7rem]" src="/hero.png" />
+          <img className="w-full animate__animated animate__fadeInRight md:w-4/5 z-50 relative lg:left-[7rem]" src="/hero.png" />
         </div>
       </div>
     </div>
