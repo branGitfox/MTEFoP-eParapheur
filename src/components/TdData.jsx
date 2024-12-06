695824029b715cb6bfc5ea5408a310e16eee6e18
BranGitfox <vixfgit@gmail.com>
2024-12-06 10:37:15 +0300

    style: commenting daisyUI CDN

--- src/components/TdData.jsx
+++ src/components/TdData.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useId, useState } from 'react'
 import { BiDotsHorizontal } from 'react-icons/bi';
 import { FaArrowUp, FaCheckCircle,FaExclamationCircle} from "react-icons/fa";
 import axiosRequest from '../axiosClient/axiosClient';
@@ -78,15 +78,74 @@ const History = ({loader, history}) => {
                     wrapperClass="absolute left-[57%] z-50" />):(
       <tr>
           <td colSpan={12} className='text-center text-gray-800'>
-              {history.map((h, index) => <HistoryData history={h}/>)}
-<ul className="steps steps-vertical">
-  <li className="step step-primary">Register</li>
-  <li className="step step-primary">Choose plan</li>
-  <li className="step">Purchase</li>
-  <li className="step">Receive Product</li>
-  <li className="step">Receive Product</li>
-</ul>
-           
+          
+              {history.map((h, index) => <HistoryData key={useId} history={h}/>)}
+
+
+
+        <div class="mt-20">
+            <ul class="">
+
+                <li class="text-left mb-10">
+                    <div class="flex flex-row items-start">
+                        <div class="flex flex-col items-center justify-center mr-5">
+                            <div
+                                class="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
+                                1
+                            </div>
+                            <span class="text-gray-500">STEP</span>
+                        </div>
+                        <div class="bg-gray-100 p-5 pb-10 ">
+                            <h4 class="text-lg leading-6 font-semibold text-gray-900">Enter Headline</h4>
+                            <p class="mt-2 text-base leading-6 text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
+                                suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li class="text-left mb-10">
+                    <div class="flex flex-row items-start">
+                        <div class="flex flex-col items-center justify-center mr-5">
+                            <div
+                                class="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
+                                2
+                            </div>
+                            <span class="text-gray-500">STEP</span>
+                        </div>
+                        <div class="bg-gray-100 p-5 pb-10 ">
+                            <h4 class="text-lg leading-6 font-semibold text-gray-900">Enter Headline</h4>
+                            <p class="mt-2 text-base leading-6 text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
+                                suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li class="text-left mb-10">
+                    <div class="flex flex-row items-start">
+                        <div class="flex flex-col items-center justify-center mr-5">
+                            <div
+                                class="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
+                                3
+                            </div>
+                            <span class="text-gray-500">STEP</span>
+                        </div>
+                        <div class="bg-gray-100 p-5 pb-10 ">
+                            <h4 class="text-lg leading-6 font-semibold text-gray-900">Enter Headline</h4>
+                            <p class="mt-2 text-base leading-6 text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
+                                suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </p>
+                        </div>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+
+ 
+   
           </td>
       </tr>
       )}
@@ -96,13 +155,8 @@ const History = ({loader, history}) => {
 
 const HistoryData = ({history}) => {
   return (
-    <>
-    <p className='text-black'>{history.created_at}</p>
-    <p>{history.ref_propre}</p>
-    <p>{history.ref_initial}</p>
-    <p>{history.name}</p>
+  <>
 
-    <hr />
   </>
   )
 }
