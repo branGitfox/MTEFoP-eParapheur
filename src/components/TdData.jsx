4464e8fac14c20b8de0dc3b5e577891956279856
BranGitfox <vixfgit@gmail.com>
2024-12-06 10:48:37 +0300

    style: deleting 2nd  and 3rd of steps

--- src/components/TdData.jsx
+++ src/components/TdData.jsx
@@ -77,25 +77,21 @@ const History = ({loader, history}) => {
                     wrapperStyle={{}}
                     wrapperClass="absolute left-[57%] z-50" />):(
       <tr>
-          <td colSpan={12} className='text-center text-gray-800'>
-          
+          <td colSpan={12} className=' text-gray-800 flex-col justify-center'>
               {history.map((h, index) => <HistoryData key={useId} history={h}/>)}
+        <div class="flex justify-center w-full">
+            <ul class="w-full">
 
-
-
-        <div class="mt-20">
-            <ul class="">
-
-                <li class="text-left mb-10">
-                    <div class="flex flex-row items-start">
-                        <div class="flex flex-col items-center justify-center mr-5">
+                <li class="text-left mb-10 w-full">
+                    <div class="flex flex-wrap lg:flex-row items-center justify-center">
+                        <div class="flex flex-row md:flex-col items-center justify-center mr-5">
                             <div
                                 class="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
                                 1
                             </div>
                             <span class="text-gray-500">STEP</span>
                         </div>
-                        <div class="bg-gray-100 p-5 pb-10 ">
+                        <div class="bg-gray-100 p-5 pb-10">
                             <h4 class="text-lg leading-6 font-semibold text-gray-900">Enter Headline</h4>
                             <p class="mt-2 text-base leading-6 text-gray-500">
                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
@@ -104,43 +100,6 @@ const History = ({loader, history}) => {
                         </div>
                     </div>
                 </li>
-                <li class="text-left mb-10">
-                    <div class="flex flex-row items-start">
-                        <div class="flex flex-col items-center justify-center mr-5">
-                            <div
-                                class="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
-                                2
-                            </div>
-                            <span class="text-gray-500">STEP</span>
-                        </div>
-                        <div class="bg-gray-100 p-5 pb-10 ">
-                            <h4 class="text-lg leading-6 font-semibold text-gray-900">Enter Headline</h4>
-                            <p class="mt-2 text-base leading-6 text-gray-500">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
-                                suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                            </p>
-                        </div>
-                    </div>
-                </li>
-                <li class="text-left mb-10">
-                    <div class="flex flex-row items-start">
-                        <div class="flex flex-col items-center justify-center mr-5">
-                            <div
-                                class="flex items-center justify-center h-20 w-20 rounded-full bg-indigo-500 text-white border-4 border-white text-xl font-semibold">
-                                3
-                            </div>
-                            <span class="text-gray-500">STEP</span>
-                        </div>
-                        <div class="bg-gray-100 p-5 pb-10 ">
-                            <h4 class="text-lg leading-6 font-semibold text-gray-900">Enter Headline</h4>
-                            <p class="mt-2 text-base leading-6 text-gray-500">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
-                                suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                            </p>
-                        </div>
-                    </div>
-                </li>
-
             </ul>
         </div>
 
