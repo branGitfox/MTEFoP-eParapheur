1b233a6bb82ce898dd1bcf39d47c8f32e02ef2ba
BranGitfox <vixfgit@gmail.com>
2024-12-13 11:00:26 +0300

    feat(lisDoc): getting user context

--- src/pages/ListDoc.jsx
+++ src/pages/ListDoc.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { IoReloadOutline } from 'react-icons/io5'
+import { userContext } from '../components/ContextWrapper'
 function ListDoc() {
-
+    const {user} = useContext(userContext)
     const handleChange = () => null
     const search = () => null
     const fresh = () => null
@@ -27,8 +28,28 @@ function ListDoc() {
           <button onClick={fresh} className="bg-blue-900 text-white flex justify-between items-center px-3   py-2 rounded-md"><IoReloadOutline className="inline mr-2"/>Actualiser</button>
       </div>
       <div className="w-full overflow-x-auto overflow-y-scroll max-h-[83%] ">
-
-</div>
+      <table className="w-full whitespace-no-wrap ">
+          <thead >
+            <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b0 bg-gray-50 ">
+              <th className="px-4 py-3 text-gray-800">Chrono</th>
+              <th className="px-4 py-3 text-gray-800">Provenance</th>
+              <th className="px-4 py-3 text-gray-800">Ref</th>
+              <th className="px-4 py-3 text-gray-800">Proprietaire</th>
+              <th className="px-4 py-3 text-gray-800">Motif</th>
+              <th className="px-4 py-3 text-gray-800">Caracteristique</th>
+              <th className="px-4 py-3 text-gray-800">Direction</th>
+              <th className="px-4 py-3 text-gray-800">Date</th>
+              <th className="px-4 py-3 text-gray-800">Livre</th>
+              <th className="px-4 py-3 text-gray-800">Porte</th>
+              {
+                user.role =='admin' && <th className="px-4 py-3 text-gray-800">Actions</th>
+              }
+              <th className="px-4 py-3 text-gray-800">Infos</th>
+              
+            </tr>
+          </thead>
+          </table>
+    </div>
     </>
 
   )
