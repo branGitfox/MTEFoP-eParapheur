32a68a8cafbdcc80aca7f5f52a6d5625411bc5da
BranGitfox <vixfgit@gmail.com>
2024-12-13 10:59:01 +0300

    feat(listDoc): addding active link to aside

--- src/components/agent/Aside.jsx
+++ src/components/agent/Aside.jsx
@@ -28,7 +28,7 @@ function Aside({toggleMenu,menu, logout, loading, user}) {
           <ul>
             <li className="relative px-6 py-3 ">
                 {
-                    location.pathname ==='/scc'?(   <span
+                    location.pathname ==='/agent'?(   <span
                 className="absolute inset-y-0 left-0 w-1 bg-[#C1AB48] rounded-tr-lg rounded-br-lg"
                 aria-hidden="true"
               ></span>):''
@@ -37,7 +37,7 @@ function Aside({toggleMenu,menu, logout, loading, user}) {
               <Link
                 className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-300 "
         
-              to='/scc'              >
+              to='/agent'              >
                 <svg
                   className="w-5 h-5"
                   aria-hidden="true"
