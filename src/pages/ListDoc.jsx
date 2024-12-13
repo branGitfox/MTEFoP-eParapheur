cbd1a9d6750ae6a7117b22d2e00341caf49a7b66
BranGitfox <vixfgit@gmail.com>
2024-12-13 11:17:08 +0300

    style(lisDoc): formating

--- src/pages/ListDoc.jsx
+++ src/pages/ListDoc.jsx
@@ -1,18 +1,18 @@
-import React, { useContext } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import { IoReloadOutline } from 'react-icons/io5'
-import { userContext } from '../components/ContextWrapper'
+import React, { useContext } from "react";
+import { FaSearch } from "react-icons/fa";
+import { IoReloadOutline } from "react-icons/io5";
+import { userContext } from "../components/ContextWrapper";
 function ListDoc() {
-    const {user} = useContext(userContext)
-    const handleChange = () => null
-    const search = () => null
-    const fresh = () => null
+  const { user } = useContext(userContext);
+  const handleChange = () => null;
+  const search = () => null;
+  const fresh = () => null;
 
   return (
     <>
-        <div className=" w-[100%]  justify-evenly flex p-3 mb-5 relative text-black">
-          <div className="w-[80%] h-12 flex justify-center relative ">
-                <input
+      <div className=" w-[100%]  justify-evenly flex p-3 mb-5 relative text-black">
+        <div className="w-[80%] h-12 flex justify-center relative ">
+          <input
             onChange={handleChange}
             value={search}
             type="text"
@@ -23,13 +23,19 @@ function ListDoc() {
             size={20}
             className="absolute  text-gray-900 right-2 top-4 "
           />
-          </div>
-      
-          <button onClick={fresh} className="bg-blue-900 text-white flex justify-between items-center px-3   py-2 rounded-md"><IoReloadOutline className="inline mr-2"/>Actualiser</button>
+        </div>
+
+        <button
+          onClick={fresh}
+          className="bg-blue-900 text-white flex justify-between items-center px-3   py-2 rounded-md"
+        >
+          <IoReloadOutline className="inline mr-2" />
+          Actualiser
+        </button>
       </div>
       <div className="w-full overflow-x-auto overflow-y-scroll max-h-[83%] ">
-      <table className="w-full whitespace-no-wrap ">
-          <thead >
+        <table className="w-full whitespace-no-wrap ">
+          <thead>
             <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b0 bg-blue-100">
               <th className="px-4 py-3 text-gray-800">Chrono</th>
               <th className="px-4 py-3 text-gray-800">Provenance</th>
@@ -40,23 +46,33 @@ function ListDoc() {
               <th className="px-4 py-3 text-gray-800">Date</th>
               <th className="px-4 py-3 text-gray-800">Livre</th>
 
-              {
-                user.role =='admin' && <th className="px-4 py-3 text-gray-800">Actions</th>
-              }
+              {user.role == "admin" && (
+                <th className="px-4 py-3 text-gray-800">Actions</th>
+              )}
             </tr>
           </thead>
           <tbody className="bg-white divide-y ">
-                 
-          <tr className="text-gray-700">
-        <td className="px-4 py-3">
-        </td>
+            <tr className="text-gray-700">
+              <td className="px-4 py-3">
+                <div className="flex items-center text-sm">
+                  <div>
+                    <p className="font-semibold">Test de td</p>
+                  </div>
+                </div>
+              </td>
+              <td className="px-4 py-3 text-sm">test</td>
+              <td className="px-4 py-3 text-sm">test</td>
+              <td className="px-4 py-3 text-sm">test</td>
+              <td className="px-4 py-3 text-sm">test</td>
+              <td className="px-4 py-3 text-sm">test</td>
+              <td className="px-4 py-3 text-sm">test</td>
+              <td className="px-4 py-3 text-sm">test</td>
             </tr>
-        </tbody>
-          </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
     </>
-
-  )
+  );
 }
 
-export default ListDoc
+export default ListDoc;
