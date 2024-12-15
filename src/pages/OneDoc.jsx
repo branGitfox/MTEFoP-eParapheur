b1aa34844daf73f4841f6fe64282e1ead425764a
BranGitfox <vixfgit@gmail.com>
2024-12-15 10:29:23 +0300

    feat(OneDoc): getOneDoc done

--- src/pages/OneDoc.jsx
+++ src/pages/OneDoc.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axiosRequest from '../axiosClient/axiosClient'
 import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
 function OneDoc() {
+
+    const [token] = useState(localStorage.getItem('ACCESS_TOKEN')) //le token d'access
+    const {id_doc} = useParams() //id du courrier dans le parametre du lien
+    const [isLoading, setIsLoading] = useState(false)
+    const [doc, setDoc] = useState({})
     //recupere un courrier par son :id
     const getOneDoc = async () => {
-        await axiosRequest.get(`/docByDirection/${id_doc}`)
+        setIsLoading(true)
+        await axiosRequest.get(`/docByDirection/${id_doc}`, {headers:{Authorization:`Bearer ${token}`, "Access-Control-Allow-Origin":"http://127.0.0.1:8000"}})
+        .then(({data}) => setDoc(data))
+        .then(() => setIsLoading(false))
+        .catch(({response}) => toast.error(response.data?.message))
+        .finally(() => setIsLoading(false))
     }
+
+    //appel de getOneDoc
+    useEffect(() => {
+        getOneDoc()
+    }, [])
+
+    console.log(doc);
+    
   return (
     <div>OneDoc</div>
   )
