a3137e191894daa3ed752bb1175723994949c8cc
BranGitfox <vixfgit@gmail.com>
2024-12-15 10:22:23 +0300

    feat(OneDoc): import useParams

--- src/pages/OneDoc.jsx
+++ src/pages/OneDoc.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import axiosRequest from '../axiosClient/axiosClient'
+import { useParams } from 'react-router-dom'
 function OneDoc() {
-
+    //recupere un courrier par son :id
     const getOneDoc = async () => {
-        
+        await axiosRequest.get(`/docByDirection/${id_doc}`)
     }
   return (
     <div>OneDoc</div>
