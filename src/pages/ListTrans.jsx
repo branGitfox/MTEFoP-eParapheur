f260a3993fabb1f93b598734b67d1428cfd6fa9a
BranGitfox <vixfgit@gmail.com>
2024-12-16 16:12:17 +0300

    feat: adding new service field to the table

--- src/pages/ListTrans.jsx
+++ src/pages/ListTrans.jsx
@@ -147,7 +147,7 @@ const DocItems = ({doc, ind}) => {
           </div>
         </td>
         <td className="px-4 py-3 text-sm">{doc.provenance}</td>
-        <td className="px-4 py-3 text-sm">{doc.ref_initial}</td>
+        <td className="px-4 py-3 text-sm">{doc.nom_serv}</td>
         <td className="px-4 py-3 text-sm">{doc.ref_initial}</td>
         <td className="px-4 py-3 text-sm">{doc.type}</td>
         <td className="px-4 py-3 text-sm">{doc.propr}</td>
