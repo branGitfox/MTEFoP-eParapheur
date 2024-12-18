952ec262ad14d8a132406394c98de308beabef12
BranGitfox <vixfgit@gmail.com>
2024-12-18 11:31:17 +0300

    feat: adding table

--- src/pages/MyDoc.jsx
+++ src/pages/MyDoc.jsx
@@ -10,6 +10,24 @@ function MyDoc() {
                 <input type="text" className='px-2 py-3 w-full border-2 border-gray-500 text-gray-900 shadow-sm focus:outline-blue-900 rounded-3xl' placeholder='la reference de votre dossier ici'/>
                 <button className='bg-blue-500 rounded-3xl px-2 py-3'>Rechercher</button>
             </div>
+            <div className="lg:w-[50%] mx-auto h-12  relative top-[68px] mt-5 px-3 flex gap-x-2 justify-evenly">
+            <table className="w-full whitespace-no-wrap ">
+          <thead >
+            <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b0 bg-gray-50 ">
+              <th className="px-4 py-3 text-gray-800">Chrono</th>
+              <th className="px-4 py-3 text-gray-800">Provenance</th>
+              <th className="px-4 py-3 text-gray-800">Ref</th>
+              <th className="px-4 py-3 text-gray-800">Proprietaire</th>
+              <th className="px-4 py-3 text-gray-800">Motif</th>
+              <th className="px-4 py-3 text-gray-800">Caracteristique</th>
+              <th className="px-4 py-3 text-gray-800">Direction</th>
+              <th className="px-4 py-3 text-gray-800">Date</th>
+              <th className="px-4 py-3 text-gray-800">Livre</th>
+              <th className="px-4 py-3 text-gray-800">Porte</th> 
+            </tr>
+          </thead>
+          </table>
+            </div>
         </div>
     </div>
   )
