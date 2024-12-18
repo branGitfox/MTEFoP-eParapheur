591e9a637de673257955718b28e637bec5cf22b7
BranGitfox <vixfgit@gmail.com>
2024-12-18 10:53:19 +0300

    feat: adding search input

--- src/pages/MyDoc.jsx
+++ src/pages/MyDoc.jsx
@@ -6,7 +6,10 @@ function MyDoc() {
         <div className="w-[100%] h-screen bg-white relative overflow-y-scroll">
             <h1 className=' relative top-[68px] text-blue-500 mt-10 text-center text-3xl font-bold'>Suivre mon dossier</h1>
             <p className='relative top-[68px] text-gray-900 text-center mt-5'>Pour suivre votre dossier il suffit d'entrer la reference dans la barre de recherche ci dessous.Puis appuyer sur le boutton "rechercher".</p>
-            <div className="lg:w-[50%] mx-auto h-12 bg-red-500 relative top-[68px] mt-5"></div>
+            <div className="lg:w-[50%] mx-auto h-12  relative top-[68px] mt-5 px-3 flex justify-evenly">
+                <input type="text" className='px-2 py-3 w-full focus:outline-blue-900'/>
+                <button className='bg-black'>Rechercher</button>
+            </div>
         </div>
     </div>
   )
