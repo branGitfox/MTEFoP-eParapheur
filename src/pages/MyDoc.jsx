a09e971a3fd04dcfec08bfd0c7a074aefe8864fe
BranGitfox <vixfgit@gmail.com>
2024-12-18 10:55:14 +0300

    feat: adding border radius

--- src/pages/MyDoc.jsx
+++ src/pages/MyDoc.jsx
@@ -7,7 +7,7 @@ function MyDoc() {
             <h1 className=' relative top-[68px] text-blue-500 mt-10 text-center text-3xl font-bold'>Suivre mon dossier</h1>
             <p className='relative top-[68px] text-gray-900 text-center mt-5'>Pour suivre votre dossier il suffit d'entrer la reference dans la barre de recherche ci dessous.Puis appuyer sur le boutton "rechercher".</p>
             <div className="lg:w-[50%] mx-auto h-12  relative top-[68px] mt-5 px-3 flex justify-evenly">
-                <input type="text" className='px-2 py-3 w-full focus:outline-blue-900'/>
+                <input type="text" className='px-2 py-3 w-full focus:outline-blue-900 rounded-l-3xl'/>
                 <button className='bg-black'>Rechercher</button>
             </div>
         </div>
