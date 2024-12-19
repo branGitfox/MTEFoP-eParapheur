f04a119dcb74ee54c1d600f2dec5f91e113f46d5
BranGitfox <vixfgit@gmail.com>
2024-12-19 13:12:03 +0300

    feat: adding search bar to admin userLis ?

--- src/components/admin/UsersList.jsx
+++ src/components/admin/UsersList.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import { FaSearch } from 'react-icons/fa'
 function UsersList() {
+    const handleChange = () => null
+    const search = null
   return (
-    <div>UsersList</div>
+   <>
+   <div className=" w-[100%]  justify-evenly flex p-3 mb-5 relative text-black">
+         <div className="w-[80%] h-12 flex justify-center relative ">
+                        <input
+                    onChange={handleChange}
+                    value={search}
+                    type="text"
+                    className="border  p-2  w-full rounded-md focus:outline-blue-900 shadow"
+                    placeholder="rechercher"
+                />
+                <FaSearch
+                    size={20}
+                    className="absolute  text-gray-900 right-2 top-4 "
+                />
+          </div>
+    </div>
+   </>
   )
 }
 
