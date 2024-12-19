36dcfffacc94a381e5ac1e07ddcaf7ca13e87a94
BranGitfox <vixfgit@gmail.com>
2024-12-19 13:19:32 +0300

    style(stats): changing the thumb background color to green

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -134,7 +134,7 @@ console.log(new Set(only));
                     <div className="bg-white border rounded shadow p-2">
                         <div className="flex flex-row items-center">
                             <div className="flex-shrink pr-4">
-                                <div className="rounded p-3 bg-yellow-600"><FaThumbsUp/></div>
+                                <div className="rounded p-3 bg-green-600"><FaThumbsUp/></div>
                             </div>
                             <div className="flex-1 text-right md:text-center">
                                 <h5 className="font-bold uppercase text-gray-500">Total Livre</h5>
