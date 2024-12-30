7e47a0f6252883c0c2f0e57003dd458b45b4a33e
BranGitfox <vixfgit@gmail.com>
2024-12-30 06:54:43 +0300

    feat(messages): adding grid div for message container

--- src/components/admin/Message.jsx
+++ src/components/admin/Message.jsx
@@ -5,8 +5,9 @@ function Message() {
     <div className='w-[95%]  bg-white mx-auto mt-4'>
         <h1 className='text-xl text-gray-900 font-semibold bg-white p-5'>Messages</h1>
         <hr className='text-gray-200 w-full border-2'/>
-        <div className="w-full py-10">
-          
+        <div className="w-full py-10 grid grid-cols-12 ">
+            <div className="col-span-6 bg-red-500 w-full">h</div>
+            <div className="col-span-6 bg-green-500 w-full">h</div>
         </div>
     </div>
   )
