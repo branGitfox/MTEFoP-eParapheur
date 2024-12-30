974d5c93d963b009798598ed8f2a78b4aef80e83
BranGitfox <vixfgit@gmail.com>
2024-12-30 07:00:06 +0300

    feat(messages): first grid done

--- src/components/admin/Message.jsx
+++ src/components/admin/Message.jsx
@@ -6,7 +6,12 @@ function Message() {
         <h1 className='text-xl text-gray-900 font-semibold bg-white p-5'>Messages</h1>
         <hr className='text-gray-200 w-full border-2'/>
         <div className="w-full py-10 grid grid-cols-12 ">
-            <div className="col-span-6 bg-red-500 w-full">h</div>
+            <div className="col-span-6 text-gray-900 px-2 w-full flex flex-col items-left">
+              <h3><strong>De: </strong>Vixfgit@gmail.com</h3>
+              <h4><strong>Objet: </strong>Erreur d'envoie</h4>
+              <h5><strong>Date d'envoie: </strong>29-12-2024</h5>
+
+            </div>
             <div className="col-span-6 bg-green-500 w-full">h</div>
         </div>
     </div>
