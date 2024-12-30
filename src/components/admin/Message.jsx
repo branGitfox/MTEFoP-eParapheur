990c8a519e52533c63b6e1a6c163a8a07c74a3f1
BranGitfox <vixfgit@gmail.com>
2024-12-30 06:51:14 +0300

    feat: adding message container

--- src/components/admin/Message.jsx
+++ src/components/admin/Message.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Message() {
   return (
-    <div>Message</div>
+    <div className='w-[95%]  bg-white mx-auto mt-4'>
+        <h1 className='text-xl text-gray-900 font-semibold bg-white p-5'>Messages</h1>
+        <hr className='text-gray-200 w-full border-2'/>
+        <div className="w-full py-10">
+          
+        </div>
+    </div>
   )
 }
-
+ 
 export default Message
