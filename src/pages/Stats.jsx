05f3640dd9f9634e725a41ed5ec90656de216ab4
BranGitfox <vixfgit@gmail.com>
2025-01-10 10:07:29 +0300

    feat(stats): adding deep stat

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -103,7 +103,7 @@ if(only.length !==0){
   return (
     <>    
         <div className="flex gap-4 items-center">
-               <h3 className='text-gray-900 text-xl text-left ml-2.5 font-semibold'>Statistiques Annuel</h3> 
+               <h3 className='text-gray-900 text-xl text-left ml-2.5 font-semibold'>Statistiques Mensuel Global</h3> 
                 <select name="date"  onChange={handleChangeDate} id="" className='text-gray-200 border p-1  rounded-xl bg-yellow-600 focus:outline-none'>
   
                     {date?.reverse()?.map((dt, index) => <option key={index} value={dt}>{dt}</option>)}
@@ -155,8 +155,51 @@ if(only.length !==0){
                 </div>     
             </div>
             <hr  className='mt-5 mb-5'/>
-            <h3 className='text-gray-900 text-xl text-left ml-2.5 font-semibold'>Graphiques</h3> 
-
+            <h3 className='text-gray-900 text-xl text-left ml-2.5 font-semibold'>Statistique Approfondie</h3> 
+            <div className="flex flex-wrap mt-5">
+                <div className="w-full md:w-1/2 xl:w-1/3 p-3">
+                   
+                    <div className="bg-white border rounded shadow p-2">
+                        <div className="flex flex-row items-center">
+                            <div className="flex-shrink pr-4">
+                                <div className="rounded p-3 bg-blue-800"><SiPaperswithcode/></div>
+                            </div>
+                            <div className="flex-1 text-right md:text-center">
+                                <h5 className="font-bold uppercase text-gray-500">Total Dossiers</h5>
+                                <h3 className="font-bold text-3xl text-gray-900">{doc.filter(dc => dc?.created_at?.substring(0, 7) == (currentDate?currentDate:doc?.reverse()[0]?.created_at?.substring(0,7))).length} <span className="text-blue-500"><i className="fas fa-caret-up"></i></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+                <div className="w-full md:w-1/2 xl:w-1/3 p-3">    
+                    <div className="bg-white border rounded shadow p-2">
+                        <div className="flex flex-row items-center">
+                            <div className="flex-shrink pr-4">
+                                <div className="rounded p-3 bg-orange-600"><SiPinboard/></div>
+                            </div>
+                            <div className="flex-1 text-right md:text-center">
+                                <h5 className="font-bold uppercase text-gray-500">Total En Attente</h5>
+                                <h3 className="font-bold text-3xl text-gray-900">{notLivred.filter(nt => nt?.created_at?.substring(0, 7) == (currentDate?currentDate:doc?.reverse()[0]?.created_at?.substring(0,7))).length}<span className="text-green-500"><i className="fas fa-caret-up"></i></span></h3>
+                            </div>
+                        </div>
+                    </div>       
+                </div>
+                <div className="w-full md:w-1/2 xl:w-1/3 p-3">  
+                    <div className="bg-white border rounded shadow p-2">
+                        <div className="flex flex-row items-center">
+                            <div className="flex-shrink pr-4">
+                                <div className="rounded p-3 bg-green-600"><FaThumbsUp/></div>
+                            </div>
+                            <div className="flex-1 text-right md:text-center">
+                                <h5 className="font-bold uppercase text-gray-500">Total Livre</h5>
+                                <h3 className="font-bold text-3xl text-gray-900">{livred.filter(lv => lv?.created_at?.substring(0, 7) == (currentDate?currentDate:doc?.reverse()[0]?.created_at?.substring(0,7))).length} <span className="text-green-500"><i className="fas fa-caret-up"></i></span></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>     
+            </div>
+            <hr  className='mt-5 mb-5'/>
         <div className="flex flex-wrap justify-evenly w-[100%] items-center gap-y-4 p-3 mt-10">
 
               <LineChart width={500} height={300} data={data}>
