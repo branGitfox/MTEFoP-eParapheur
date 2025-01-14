07a07fe55cf16af54c54cec2ed3c6fd69fc69887
BranGitfox <vixfgit@gmail.com>
2025-01-14 09:44:08 +0300

    refactor: changing the stepper design

--- src/pages/MyDoc.jsx
+++ src/pages/MyDoc.jsx
@@ -63,29 +63,81 @@ function MyDoc() {
             <History history={history} loader={historyLoader} all={false} /></div>):""} */}
 
             {/* tailwind stepper  */}
-<ol class="flex items-center w-full relative top-[68px] justify-center bg-red-500">
-    <li class="flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800">
-        <span class="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
-            <svg class="w-3.5 h-3.5 text-blue-600 lg:w-4 lg:h-4 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-            </svg>
-        </span>
-    </li>
-    <li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
-        <span class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-            <svg class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                <path d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z"/>
-            </svg>
-        </span>
-    </li>
-    <li class="flex items-center w-full">
-        <span class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-            <svg class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v3H7V2Zm5.7 8.289-3.975 3.857a1 1 0 0 1-1.393 0L5.3 12.182a1.002 1.002 0 1 1 1.4-1.436l1.328 1.289 3.28-3.181a1 1 0 1 1 1.392 1.435Z"/>
-            </svg>
-        </span>
-    </li>
-</ol>
+  <ul class="relative flex flex-row gap-x-2 top-[68px] bg-red-500">
+
+  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-neutral-700 dark:text-white">
+        1
+      </span>
+      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+        Step
+      </span>
+    </div>
+    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+  </li>
+
+
+  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-neutral-700 dark:text-white">
+        2
+      </span>
+      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+        Step
+      </span>
+    </div>
+    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+  </li>
+
+  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-neutral-700 dark:text-white">
+        2
+      </span>
+      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+        Step
+      </span>
+    </div>
+    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+  </li>
+  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-neutral-700 dark:text-white">
+        2
+      </span>
+      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+        Step
+      </span>
+    </div>
+    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+  </li>
+  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-neutral-700 dark:text-white">
+        2
+      </span>
+      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+        Step
+      </span>
+    </div>
+    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+  </li>
+
+
+  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-neutral-700 dark:text-white">
+        3
+      </span>
+      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+        Step
+      </span>
+    </div>
+    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+  </li>
+ 
+</ul>
 
 {/* fin tailwind stepper */}
 
