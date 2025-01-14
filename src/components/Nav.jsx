4c409c5b55337339283b63e0b7c25edf0a206104
BranGitfox <vixfgit@gmail.com>
2025-01-14 15:27:41 +0300

    style: changing positioni fixed to sticky

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ function Nav() {
     <>
       <nav
         id="header"
-        className="sticky top-0 w-full z-50 top-0 text-white bg-[#191970]"
+        className="fixed top-0 w-full z-50  text-white bg-[#191970]"
       >
         <div className="w-full  mx-auto flex flex-wrap items-center justify-between mt-0 py-2 ">
           <div className="pl-4 flex items-center">
