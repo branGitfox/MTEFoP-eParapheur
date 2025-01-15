0e4b1d4ed269f87f438f28bde3093c0865be1500
BranGitfox <vixfgit@gmail.com>
2025-01-15 10:58:39 +0300

    fix: closing tag missed

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -353,46 +353,8 @@ console.log(docByDirection);
               
         </div>))}
 
-        
-              
-        {/* FIN par Direction */}
-           
-              
-        </div>
-        <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-          <div className="bg-white border rounded shadow p-2">
-            <div className="flex flex-row items-center">
-              <div className="flex-shrink pr-4">
-                <div className="rounded p-3 bg-blue-400">
-                  <FaHouseChimney />
-                </div>
-              </div>
-              <div className="flex-1 text-right md:text-center">
-                <h5 className="font-bold uppercase text-gray-500">
-                  DIR
-                </h5>
-                <h3 className="font-bold text-3xl text-gray-900">
-                  {
-                    livred.filter(
-                      (lv) =>
-                        lv?.created_at?.substring(0, 7) ==
-                        (currentDate
-                          ? currentDate
-                          : livred?.created_at?.substring(0, 7))
-                    ).length
-                  }{" "}
-                  <span className="text-green-500">
-                    <i className="fas fa-caret-up"></i>
-                  </span>
-                </h3>
-              </div>
-              
-            </div>
-              
-          </div>
-              {/*FIN  par direction */}
-              
-        </div>
+
+      {/* FIN par Direcion */}
       </div>
       <hr className="mt-5 mb-5" />
 
