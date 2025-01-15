a3b70f4f0d508452d3889ab75c00c2bfc72cd93a
BranGitfox <vixfgit@gmail.com>
2025-01-15 10:55:44 +0300

    feat: mapping docByDirection

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -181,6 +181,7 @@ let index = 0
 
 
 
+console.log(docByDirection);
 
  
   return (
@@ -323,7 +324,7 @@ let index = 0
               
         </div>
               {/* par direction */}
-              <div className="w-full md:w-1/2 xl:w-1/3 p-3">
+              {docByDirection.map((doc, index) => (              <div className="w-full md:w-1/2 xl:w-1/3 p-3">
           <div className="bg-white border rounded shadow p-2">
             <div className="flex flex-row items-center">
               <div className="flex-shrink pr-4">
@@ -333,18 +334,11 @@ let index = 0
               </div>
               <div className="flex-1 text-right md:text-center">
                 <h5 className="font-bold uppercase text-gray-500">
-                    Ministre
+                    {doc[0]}
                 </h5>
                 <h3 className="font-bold text-3xl text-gray-900">
-                  {
-                    livred.filter(
-                      (lv) =>
-                        lv?.created_at?.substring(0, 7) ==
-                        (currentDate
-                          ? currentDate
-                          : livred?.created_at?.substring(0, 7))
-                    ).length
-                  }{" "}
+      
+                  {doc[1]}
                   <span className="text-green-500">
                     <i className="fas fa-caret-up"></i>
                   </span>
@@ -357,71 +351,11 @@ let index = 0
           
               
               
-        </div>
-        <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-          <div className="bg-white border rounded shadow p-2">
-            <div className="flex flex-row items-center">
-              <div className="flex-shrink pr-4">
-                <div className="rounded p-3 bg-purple-600">
-                  <FaHouseChimneyWindow />
-                </div>
-              </div>
-              <div className="flex-1 text-right md:text-center">
-                <h5 className="font-bold uppercase text-gray-500">
-                  FOP
-                </h5>
-                <h3 className="font-bold text-3xl text-gray-900">
-                  {
-                    livred.filter(
-                      (lv) =>
-                        lv?.created_at?.substring(0, 7) ==
-                        (currentDate
-                          ? currentDate
-                          : livred?.created_at?.substring(0, 7))
-                    ).length
-                  }{" "}
-                  <span className="text-green-500">
-                    <i className="fas fa-caret-up"></i>
-                  </span>
-                </h3>
-              </div>
-              
-            </div>
-              
-          </div>
+        </div>))}
 
-        </div>
-        <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-          <div className="bg-white border rounded shadow p-2">
-            <div className="flex flex-row items-center">
-              <div className="flex-shrink pr-4">
-                <div className="rounded p-3 bg-blue-600">
-                  <FaHouseChimneyUser />
-                </div>
-              </div>
-              <div className="flex-1 text-right md:text-center">
-                <h5 className="font-bold uppercase text-gray-500">
-                 DIRCAB
-                </h5>
-                <h3 className="font-bold text-3xl text-gray-900">
-                  {
-                    livred.filter(
-                      (lv) =>
-                        lv?.created_at?.substring(0, 7) ==
-                        (currentDate
-                          ? currentDate
-                          : livred?.created_at?.substring(0, 7))
-                    ).length
-                  }{" "}
-                  <span className="text-green-500">
-                    <i className="fas fa-caret-up"></i>
-                  </span>
-                </h3>
-              </div>
+        
               
-            </div>
-              
-          </div>
+        {/* FIN par Direction */}
            
               
         </div>
