71af6aac841f2d35de0448898207ef643019c401
BranGitfox <vixfgit@gmail.com>
2025-01-20 08:52:16 +0300

    comment: adding coment

--- src/components/admin/Message.jsx
+++ src/components/admin/Message.jsx
@@ -47,6 +47,7 @@ function Message() {
   )
 }
 
+//component pour afficher le corps du message avec un effet toggle
 const ShowMessage = ({mess}) =>  {
   const [show, setShow] = useState(false)
 
@@ -62,7 +63,7 @@ const ShowMessage = ({mess}) =>  {
               <h5><strong>Date d'envoie: </strong>{mess.created_at}</h5>
             </div>
             <div className="col-span-6  w-full flex flex-col items-end px-2 justify-center">
-               <Link onClick={toggle} className='text-gray-900 underline'>Lire<FaReadme size={25} className='text-gray-500 inline ml-2'/></Link>
+               <Link onClick={toggle} className='text-gray-900 underline'>{show?"Lu":"Lire"}<FaReadme size={25} className='text-gray-500 inline ml-2'/></Link>
             </div>
             {show&& <div className="col-span-12 p-4 text-center font-normal text-gray-800">
            <hr className='text-gray-200 w-full border-2 mb-10'/>
