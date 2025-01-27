79340d0b1dd376989e1d0c1dfaf203185b6a97f4
BranGitfox <vixfgit@gmail.com>
2025-01-27 09:34:21 +0300

    feat: adding filter to the spstats

--- src/components/sp/SpStats.jsx
+++ src/components/sp/SpStats.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axiosRequest from '../../axiosClient/axiosClient'; 
-import { FaHouseFire } from 'react-icons/fa6';
+import { FaHouseFire, FaFilter } from 'react-icons/fa6';
 function SpStats() {
     const [docByService, setDocByService] = useState([])
     const [token] = useState(localStorage.getItem('ACCESS_TOKEN')) //token d'access
@@ -22,9 +22,25 @@ function SpStats() {
           }, [])
   return (
     <>
+        <div className="flex w-full flex-wrap md:w-[400px] justify-between px-1 ">
+          <h2 className="text-gray-800 font-bold">
+             <FaFilter className="inline mr-1 text-blue-500 "/> Fitres
+          </h2>
+          <form onSubmit={null} className=" w-full md:w-[200px] flex mt-2  lg:mt-0 justify-evenly gap-x-5 ">
+                  <div >
+                  <label htmlFor="" className="text-gray-800 font-medium" >Debut</label>
+                    <input className="text-gray-900 p-2 rounded-md w-full"  onChange={null} name="start" type="date"  />
+                  </div>
+                  <div >
+                  <label htmlFor="" className="text-gray-800 font-medium">Fin</label>
+                  <input  className="text-gray-800 p-2 rounded-md w-full" onChange={null} name="end"  type="date"  />
+                  </div>
+                  <button className="bg-blue-600 px-3 h-10 relative top-6 rounded-md" type="submit">Valider</button>
+                </form>
+        </div>
       <div className="flex  flex-wrap w-full justify-center  p-3">
 
-        <div>SpStats</div>
+     
                 {docByService.map((doc, index) => (
                   <div className="w-[300px]">
                   <div className="border-2 border-gray-400 border-dashed hover:border-transparent hover:bg-white hover:shadow-xl rounded p-6 m-2 md:mx-10 md:my-6">
