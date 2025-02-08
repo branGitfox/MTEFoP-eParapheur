4889f213560a74350920ecc5177a147da5f37024
BranGitfox <vixfgit@gmail.com>
2025-02-08 11:13:15 +0300

    feat: updating feature done

--- src/pages/UpDoc.jsx
+++ src/pages/UpDoc.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState, useEffect } from 'react';
 import { BeatLoader } from 'react-spinners';
 import { Oval } from 'react-loader-spinner';
+import { useNavigate, useParams } from 'react-router-dom';
+import axiosRequest from '../axiosClient/axiosClient';
+import { toast } from 'react-toastify';
+import { userContext } from '../components/ContextWrapper';
 function UpDoc() {
       const [isLoading, setIsLoading] = useState(false); //etat du loader
+      const {user} = useContext(userContext)
       const [token] = useState(localStorage.getItem("ACCESS_TOKEN")); //le token d'acces
+    //   const [doc, setDoc] = useState({})
         const [waiting, setWaiting] = useState(false); // looder pour la recuperation des directions
           const [dir, setDir] = useState([]); //liste des directions
+          const {id_doc} = useParams()
+      const [formData, setFormData] = useState({  status: "non reçu",
+        transfere:"non",
+        user_id: user.id,})
+
+        const Navigate = useNavigate()
           //recuperation de la liste de Direction
   const getDir = async () => {
     setWaiting(true);
@@ -22,19 +34,53 @@ function UpDoc() {
       toast.error("Verifiez votre connexion internet")
     }
 
-      //appel a la fonction getDir dependant du chemin grace a (useLocation)
-      useEffect(() => {
-        getDir()
-      }, [location.pathname])
+
  
   };
+        //appel a la fonction getDir dependant du chemin grace a (useLocation)
+      useEffect(() => {
+        getDir()
+        getDoc()
+      }, [location.pathname])
+
+      const getDoc = async () => {
+        try{
+            await axiosRequest.get(`/getDoc/${id_doc}`, {headers:{Authorization:`Bearer ${token}`, "Access-Control-Allow-Origin":"http://127.0.0.1:8000"}})
+            .then(({data}) => setFormData(data))
+            .catch((err) => console.log(err))
+        }catch(err) {
+            console.log(err);   
+        }
+
+
+      }
+
+
+    const handlechange = (e) => {
+        const {name, value} = e.target
+        setFormData((formData) => ({...formData, [name]:value}))
+    }
+     console.log(formData);
+     
+      const submit = async () => {
+        try{
+            await axiosRequest.post(`/upDoc/${id_doc}`, formData, {headers:{Authorization:`Bearer ${token}`, "Access-Control-Allow-Origin":"http://127.0.0.1:8000"}})
+            .then(({data}) => toast.success(data.message))
+            .then(() => Navigate('/'))
+            .catch((err) => toast.error(err.response?.data?.message))
+        }catch(err){
+            console.log("");
+            
+        }
+      }
+      
   return (
     <>
     <h3 className="text-gray-900 text-2xl ml-2.5 font-semibold ">
       Modification d'un Dossier
     </h3>
     <form
-      onSubmit={null}
+      onSubmit={submit}
       className="w-[100%]  m-auto  p-10 bg-white rounded-md shadow mt-5 md:mt-10"
     >
       <div className="mb-5">
@@ -46,11 +92,12 @@ function UpDoc() {
         </label>
         <input
           type="text"
-          onChange={null}
+          onChange={handlechange}
           id="chrono"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-900  focus:border-blue-500 block w-full p-2.5  "
           placeholder="reference du dossier"
           name="chrono"
+          value={formData.chrono}
         />
       </div>
       <div className="mb-5">
@@ -63,10 +110,11 @@ function UpDoc() {
         <input
           type="text"
           id="ref"
-          onChange={null}
+          onChange={handlechange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-900  focus:border-blue-500 block w-full p-2.5  "
           placeholder="chrono initial"
           name="ref"
+          value={formData.ref}
         />
       </div>
       <div className="mb-5">
@@ -78,11 +126,12 @@ function UpDoc() {
         </label>
         <input
           type="text"
-          onChange={null}
+          onChange={handlechange}
           id="provenance"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-900  focus:border-blue-500 block w-full p-2.5  "
           placeholder="Provenance"
           name="provenance"
+          value={formData.provenance}
         />
       </div>
       <div className="mb-5">
@@ -94,11 +143,12 @@ function UpDoc() {
         </label>
         <input
           type="text"
-          onChange={null}
+          onChange={handlechange}
           id="prop"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-900  focus:border-blue-500 block w-full p-2.5  "
           placeholder="proprietaire du dossier"
           name="proprietaire"
+          value={formData?.proprietaire}
         />
       </div>
       <div className="mb-5">
@@ -110,11 +160,12 @@ function UpDoc() {
         </label>
         <input
           type="text"
-          onChange={null}
+          onChange={handlechange}
           id="motif"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-900  focus:border-blue-500 block w-full p-2.5   "
           placeholder="objet du depot"
           name="motif"
+          value={formData.motif}
         />
       </div>
       <div className="mb-5">
@@ -125,14 +176,14 @@ function UpDoc() {
           Caracteristique
         </label>
         <select
-          onChange={null}
+          onChange={handlechange}
           className="text-gray-900 w-full p-2 rounded"
           name="caracteristique"
           id=""
         >
           <option value="">- Selectionner ici -</option>
-          <option value="plis ferme">Plis Ferme</option>
-          <option value="normale">Normale</option>
+          <option value="plis ferme" selected={formData.caracteristique == "plis ferme"?true:false}>Plis Ferme</option>
+          <option value="normale" selected={formData.caracteristique == "normale"?true:false}>Normale</option>
         </select>
       </div>
       <div className="mb-5">
@@ -154,14 +205,14 @@ function UpDoc() {
           />
         ) : (
           <select
-            onChange={null}
+            onChange={handlechange}
             className="text-gray-900 w-full p-2 rounded"
             name="dir_id"
             id=""
           >
             <option value="">- Selectionner ici -</option>
             {dir.map((d, index) => (
-              <option value={d.d_id} key={index}>
+              <option value={d.d_id} selected={d.d_i == formData.d_id ?true:false} key={index}>
                 {d.nom_dir}
               </option>
             ))}
@@ -178,10 +229,11 @@ function UpDoc() {
         <input
           type="text"
           id="cin"
-          onChange={null}
+          onChange={handlechange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-900  focus:border-blue-500 block w-full p-2.5  "
           placeholder="Votre CIN"
           name="cin"
+          value={formData.cin}
         />
       </div>
       <div className="mb-5">
@@ -194,10 +246,11 @@ function UpDoc() {
         <input
           type="text"
           id="tel"
-          onChange={null}
+          onChange={handlechange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-900  focus:border-blue-500 block w-full p-2.5  "
           placeholder="Votre tel"
           name="tel"
+          value={formData.tel}
         />
       </div>
       <button
