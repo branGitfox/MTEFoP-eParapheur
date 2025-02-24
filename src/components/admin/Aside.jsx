23d7ed1ca421b1965bda06d51c2858ee992952b0
BranGitfox <vixfgit@gmail.com>
2025-02-24 10:43:02 +0300

    feat: add menu toggle to admin responsivity and fixing somem design bug no more description

--- src/components/admin/Aside.jsx
+++ src/components/admin/Aside.jsx
@@ -10,6 +10,7 @@ import { toast } from "react-toastify";
 function Aside() {
   const locations = useLocation();
   const [loading, setIsLoading] = useState(false);
+  const [show, setShow] = useState(false)
 
   const logout = async () => {
     setIsLoading(true);
@@ -32,7 +33,7 @@ function Aside() {
   };
   return (
     <>
-      <aside className="relative h-screen w-[295px] hidden sm:block shadow-xl bg-[#191970] rounded-r-xl ">
+      <aside className="relative h-screen w-[295px] hidden lg:block shadow-xl bg-[#191970] rounded-r-xl ">
         <div className="p-3 ">
         <img src="/mtefp_logo.jpeg " className='w-10 h-10 rounded-full inline ml-2' alt="" />
 
@@ -143,6 +144,117 @@ function Aside() {
             </button>
           </div>
       </aside>
+      {show && (      <aside className="absolute h-screen w-[250px]  shadow-xl bg-[#191970] rounded-r-xl z-50">
+        <div className="p-3 ">
+        <img src="/mtefp_logo.jpeg " className='w-10 h-10 rounded-full inline ml-2' alt="" />
+
+          <Link
+            to="/admin"
+            className="text-white text-xl ml-5 font-semibold  hover:text-gray-300"
+          >
+         
+            e-Parapheur
+          </Link>
+        </div>
+        <nav className="text-white text-normal font-semibold pt-3">
+          <Link
+            to="/admin"
+            className={`flex items-center relative rounded-sm  hover:bg-blue-900  text-white py-4 pl-6 nav-item`}
+          >
+               {
+              locations.pathname === "/admin" ? (
+                <span
+                  class="absolute inset-y-0 left-0 w-2 bg-[#C1AB48] rounded-tr-lg rounded-br-lg"
+                  aria-hidden="true"
+                ></span>
+              ) : (
+                ""
+              )
+            } <FaChartLine  className="mr-4" />
+            Tableau de Bord
+          </Link>
+          <Link
+            to="/admin/userregister"
+            className={`flex relative items-center rounded-sm  text-white hover:bg-blue-900 py-4 pl-6 nav-item`}
+          >
+            {
+              locations.pathname === "/admin/userregister" ? (
+                <span
+                  class="absolute inset-y-0 left-0 w-2 bg-[#C1AB48] rounded-tr-lg rounded-br-lg"
+                  aria-hidden="true"
+                ></span>
+              ) : (
+                ""
+              )
+            }<FaUserPlus className="mr-4" />
+            Ajout Utilisateur
+          </Link>
+          <Link
+            to="/admin/usersList"
+            className={`flex relative items-center rounded-sm  text-white hover:bg-blue-900 py-4 pl-6 nav-item`}
+          >
+            {
+              locations.pathname === "/admin/usersList" ? (
+                <span
+                  class="absolute inset-y-0 left-0 w-2 bg-[#C1AB48] rounded-tr-lg rounded-br-lg"
+                  aria-hidden="true"
+                ></span>
+              ) : (
+                ""
+              )
+            }<FaUsers className="mr-4" />
+            Liste Utilisateurs
+          </Link>
+          <Link
+            to="/admin/sccservdirdg"
+            className={`flex relative items-center rounded-sm  text-white hover:bg-blue-900 py-4 pl-6 nav-item`}
+          >
+               {
+              locations.pathname === "/admin/sccservdirdg" ? (
+                <span
+                  class="absolute inset-y-0 left-0 w-2 bg-[#C1AB48] rounded-tr-lg rounded-br-lg"
+                  aria-hidden="true"
+                ></span>
+              ) : (
+                ""
+              )
+            } <FaHouseLaptop  className="mr-4" />
+            Ajout DG/Dir/Serv
+          </Link>
+          <Link
+            to="/admin/messages"
+            className={`flex relative items-center rounded-sm  text-white hover:bg-blue-900 py-4 pl-6 nav-item`}
+          >
+               {
+              locations.pathname === "/admin/messages" ? (
+                <span
+                  class="absolute inset-y-0 left-0 w-2 bg-[#C1AB48] rounded-tr-lg rounded-br-lg"
+                  aria-hidden="true"
+                ></span>
+              ) : (
+                ""
+              )
+            } <FaMessage  className="mr-4" />
+            Messages
+          </Link>
+          <Link
+            to="/scc"
+            className="flex items-center text-white rounded-sm hover:bg-blue-900 py-4 pl-6 nav-item"
+          >
+            <FaMailBulk  className="mr-4" />
+            Visiter Service-CC
+          </Link>
+        </nav>
+        <div className="px-6 my-6 absolute bottom-0">
+            <button
+            onClick={logout}
+              className="flex items-center justify-between w-full px-2 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-[#A10304] border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+            >
+             {loading?(<BeatLoader size={15} color='yellow'/>):'Se Deconnecter'}
+             <FaArrowRight   className='ml-[3rem]'/>
+            </button>
+          </div>
+      </aside>)}
     </>
   );
 }
--- src/components/admin/Header.jsx
+++ src/components/admin/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <>
     
-    <header className="w-full items-center bg-white py-1 px-6 hidden sm:flex">
+    <header className="w-full items-center bg-white py-1 px-6 flex">
             <div className="w-1/2 text-gray-900 font-semibold">{location.pathname}</div>
             <div className="relative w-1/2 flex justify-end">
                 <button  className="realtive z-10 w-10 h-10 rounded-full overflow-hidden border-4 border-gray-200 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
