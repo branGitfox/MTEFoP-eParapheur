a823a281668a1f63022d2cfab9b14c580ee900c7
BranGitfox <vixfgit@gmail.com>
2025-05-14 16:41:13 +0300

    fix(listtrans2): filter error

--- src/pages/ListDoc2.jsx
+++ src/pages/ListDoc2.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { FaCheck, FaSearch } from "react-icons/fa";
+import {FaArrowLeft, FaArrowRight, FaCheck, FaSearch} from "react-icons/fa";
 import { IoReloadOutline } from "react-icons/io5";
 import { userContext } from "../components/ContextWrapper";
 import { BiTransfer } from "react-icons/bi";
@@ -17,11 +17,14 @@ function ListDoc2() {
   const [search, setSearch] = useState("")
     const [updateLivre, setUpdateLivre] = useState(false)
     const navigate = useNavigate()
+  const [page, setPage] = useState(1)
+
+  const [lines, setLines] = useState("all")
   //recupere la liste des courrier par direction
   const fetchByDirection = async () => {
     setLoader(true)
     try{
-      await axiosRequest.get('/moveByService', {headers:{Authorization:`Bearer ${token}`}})
+      await axiosRequest.get(`/moveByService?page=${page}&lines=${lines}`, {headers:{Authorization:`Bearer ${token}`}})
       .then(({data}) => setDocsByDirection(data))
       .then(() => setLoader(false))
       .catch((err) => console.log(""))
@@ -34,29 +37,31 @@ function ListDoc2() {
 
 
 
-  //appelle de fechByDirection
-  useEffect(() => {
-    fetchByDirection()
-  }, [reload, updateLivre])
+
 
   //filtre la barre de recherche
-  const filtered = docsByDirection.filter((doc) => {
-    if (doc.propr?.toLowerCase().includes(search?.toLowerCase())) {
+  const filtered = docsByDirection?.data?.filter((doc) => {
+    if (doc?.propr?.toLowerCase().includes(search?.toLowerCase())) {
       return true;
     }
 
-    if (doc.chrono?.toLowerCase().includes(search?.toLowerCase())) {
+    if (doc?.chrono?.toLowerCase().includes(search?.toLowerCase())) {
       return true;
     }
 
-    if (doc.ref?.toLowerCase().includes(search?.toLowerCase())) {
+    if (doc?.ref?.toLowerCase().includes(search?.toLowerCase())) {
       return true;
     }
 
-    if (doc.created_at?.toLowerCase().includes(search?.toLowerCase())) {
+    if (doc?.created_at?.toLowerCase().includes(search?.toLowerCase())) {
       return true;
     }
 })
+  //appelle de fechByDirection
+  useEffect(() => {
+    fetchByDirection()
+  }, [reload, updateLivre, lines, page])
+  console.log(filtered)
 
 //prend la valeur de la barre de recherche
   const handleChange = (e) => {
@@ -70,12 +75,83 @@ function ListDoc2() {
   const showDocByOne = (id_doc) => {
     navigate(`/agent/doc/${id_doc}`)
   }
+  const changeLine = (e) => {
+    setLines(e.target.value)
+  }
 
-  filtered.reverse()
+  const nextPage = () => {
+    setPage((page) => page + 1)
+  }
+
+  const previousPage = () => {
+    setPage((page) => page - 1)
+  }
+
+  const gotoPage = (page) => {
+    setPage(page)
+  }
+
+  const pageLinks = []
+
+  for(let i =1; i<= docsByDirection?.last_page;i++) {
+    pageLinks.push({number:i})
+  }
+  filtered?.reverse()
   return (
     <>
-      <div className=" w-[100%]  justify-evenly flex p-3 mb-5 relative text-black">
-        <div className="w-[80%] h-12 flex justify-center relative ">
+      <div className=" w-[100%]  justify-start gap-x-4  flex p-3 mb-5 relative text-black">
+        <div className="relative">
+              <span className="absolute inset-y-0 left-0 justify-start flex items-center pl-3">
+            <svg className="w-full h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
+                <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+        </span>
+
+
+          <input value={search}   onChange={handleChange} type="text" className="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-md focus:border-blue-400  focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" placeholder="Rechercher"/>
+        </div>
+        <button onClick={fresh} className="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+          <svg className="w-5 h-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
+          </svg>
+
+          <span className="mx-1">Rafraichir</span>
+        </button>
+        <div className="flex items-center me-4">
+          {/* <input id="inline-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500   focus:ring-2"/> */}
+          <select onChange={changeLine} className="py-3 px-4 pe-9 block w-full bg-white shadow-lg border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+            <option value='all'>Afficher toutes les lignes</option>
+            <option value='15'>15 lignes</option>
+            <option value='50'>50 lignes</option>
+            <option value='100'>100 lignes</option>
+          </select>
+          {/* <label for="inline-checkbox" class="ms-2 text-sm font-medium text-gray-è00 ">Afficher Tout</label> */}
+        </div>
+        <div className="flex">
+          <button disabled={page==1?true:false} onClick={previousPage}  className={`flex items-center px-4 py-2 mx-1 text-gray-500  ${page==1?'bg-gray-200 text-gray-500':'bg-blue-600 text-white'} rounded-md cursor-not-allowed`}>
+            <FaArrowLeft className="mr-1"/> Précédent
+          </button>
+
+          {pageLinks.map((number, index) => (
+              <button key={index}  onClick={() => gotoPage(number.number)} className={`items-center hidden px-4 py-2 mx-1 text-gray-700 ${page!==number.number?'bg-white text-gray-500':'bg-blue-600 text-white'} transition-colors duration-300 transform  rounded-md sm:flex`}>
+                {number.number}
+              </button>
+          ))}
+
+
+          {/* <a href="#" class="items-center hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:flex   hover:bg-blue-600  hover:text-white ">
+            2
+        </a>
+
+        <a href="#" class="items-center hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:flex  hover:bg-blue-600  hover:text-white ">
+            3
+        </a> */}
+
+          <button disabled={page==docsByDirection?.last_page?true:false}  onClick={nextPage} className={`flex items-center px-4 py-2 mx-1  ${page<docsByDirection?.last_page?'bg-blue-600 text-white':'bg-gray-200 text-gray-700'} transition-colors duration-300 transform  rounded-md`}>
+            Suivant<FaArrowRight className="ml-1"/>
+          </button>
+        </div>
+        {/* <div className="w-[80%] h-12 flex justify-center relative ">
           <input
             onChange={handleChange}
             value={search}
@@ -87,15 +163,15 @@ function ListDoc2() {
             size={20}
             className="absolute  text-gray-600 right-2 top-4 "
           />
-        </div>
+        </div> */}
 
-        <button
+        {/* <button
           onClick={fresh}
           className="bg-blue-900 text-white flex justify-between items-center px-3   py-2 rounded-md"
         >
           <IoReloadOutline className="inline mr-2" />
           Actualiser
-        </button>
+        </button> */}
       </div>
       <div className="w-full overflow-x-auto overflow-y-scroll max-h-[83%] ">
         <table className="w-full whitespace-no-wrap ">
@@ -143,7 +219,7 @@ function ListDoc2() {
 const DocByDirection = ({docsByDirection, token, updateLivre, setUpdateLivre, showDocByOne}) => {
     return (
         <>
-         {docsByDirection.reverse().map((doc, index) => <DocItems key={index} token={token} doc={doc} ind={index} showDocByOne={showDocByOne} setUpdateLivre={setUpdateLivre} updateLivre={updateLivre}/>)}
+         {docsByDirection?.reverse()?.map((doc, index) => <DocItems key={index} token={token} doc={doc} ind={index} showDocByOne={showDocByOne} setUpdateLivre={setUpdateLivre} updateLivre={updateLivre}/>)}
         </>
     )
 }
